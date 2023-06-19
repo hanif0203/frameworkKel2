b23ea39e2ca789e0e797d937829ac156093cac6f
hanif bahy hasyid <hanifbahyhasid@gmail.com>
2023-06-19 09:26:20 +0700

    mengubah edit dan show

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -77,7 +77,12 @@ class EmployeeController extends Controller
      */
     public function show(string $id)
     {
-        //
+    $pageTitle = 'Employee Detail';
+
+    // ELOQUENT
+    $employee = Employee::find($id);
+
+    return view('employee.show', compact('pageTitle', 'employee'));
     }
 
     /**
@@ -85,7 +90,14 @@ class EmployeeController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $pageTitle = 'Edit Employee';
+
+        // ELOQUENT
+        $positions = Position::all();
+        $employee = Employee::find($id);
+    
+        return view('employee.edit', compact('pageTitle', 'positions', 'employee'));
+    
     }
 
     /**
@@ -93,8 +105,35 @@ class EmployeeController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $messages = [
+            'required' => ':Attribute harus diisi.',
+            'email' => 'Isi :attribute dengan format yang benar',
+            'numeric' => 'Isi :attribute dengan angka'
+        ];
+    
+        $validator = Validator::make($request->all(), [
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'email' => 'required|email',
+            'age' => 'required|numeric',
+        ], $messages);
+    
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+    
+        // ELOQUENT
+        $employee = Employee::find($id);
+        $employee->firstname = $request->firstName;
+        $employee->lastname = $request->lastName;
+        $employee->email = $request->email;
+        $employee->age = $request->age;
+        $employee->position_id = $request->position;
+        $employee->save();
+    
+        return redirect()->route('employees.index');
     }
+    
 
     /**
      * Remove the specified resource from storage.
