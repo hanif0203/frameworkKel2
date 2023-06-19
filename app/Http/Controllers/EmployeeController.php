fa12a934769da0d876ec5c662612837d51aeb0ae
hanif bahy hasyid <hanifbahyhasid@gmail.com>
2023-06-19 09:53:41 +0700

    menambahkan fitur delet

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -140,6 +140,9 @@ class EmployeeController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        //ELOQUENT
+        Employee::find($id)->delete();
+
+        return redirect()->route('employees.index');
     }
 }
